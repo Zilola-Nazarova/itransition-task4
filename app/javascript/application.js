acdcbc32ddcd147e797ab5acbcb135bcb42df90b
zilola <zilolanazarova1994@gmail.com>
2024-04-20 01:52:31 +0500

    Create react entry point, redux store, sample redux slice and tremporary react components

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,23 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+// import "bootstrap";
+// import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import store from './redux/store';
+import { Provider } from "react-redux";
+import App from './components/App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+);
---
+++ app/javascript/components/App.js
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getGreetings } from '../redux/greetings/greetingsSlice';
+
+import Greeting from './Greeting';
+import Counter from './Counter';
+
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getGreetings());
+  }, [dispatch]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Greeting />} />
+      <Route path="/counter" element={<Counter />} />
+    </Routes>
+  );
+}
+
+export default App;
