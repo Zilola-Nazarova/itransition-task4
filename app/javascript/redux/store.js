3322a9cf192c0e8cfcc07d1c148105803d4f3fdc
zilola <zilolanazarova1994@gmail.com>
2024-04-20 23:52:01 +0500

    Create Users controller, userSlice and set up the store, make sure the front-end gets the succesfull responce

--- app/javascript/redux/store.js
+++ app/javascript/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { logger } from 'redux-logger';
 import greetingsReducer from './greetings/greetingsSlice';
+import usersReducer from './users/usersSlice';
 
 const store = configureStore({
   reducer: {
     greetings: greetingsReducer,
+    users: usersReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
---
+++ app/javascript/redux/users/usersSlice.js
@@ -0,0 +1,50 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const USERS_URL = 'http://127.0.0.1:3000/api/v1/user';
+
+export const getUsers = createAsyncThunk(
+  'users/getUsers',
+  async (_, thunkAPI) => {
+    try {
+      const resp = await axios.get(USERS_URL);
+      return resp.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  },
+);
+
+const initialState = {
+  message: '',
+  isLoading: false,
+  error: undefined,
+};
+
+export const usersSlice = createSlice({
+  name: 'users',
+  initialState,
+  reducers: { },
+  extraReducers(builder) {
+    builder
+      .addCase(getUsers.pending, (state) => {
+        const temp = state;
+        temp.isLoading = true;
+        return temp;
+      })
+      .addCase(getUsers.fulfilled, (state, action) => {
+        const temp = state;
+        temp.isLoading = false;
+        temp.message = action.payload;
+        return temp;
+      })
+      .addCase(getUsers.rejected, (state, action) => {
+        const temp = state;
+        temp.isLoading = false;
+        temp.error = action.payload.message;
+        return temp;
+      });
+  },
+});
+
+export default usersSlice.reducer;
