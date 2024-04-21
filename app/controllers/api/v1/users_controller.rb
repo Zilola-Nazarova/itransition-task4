89e956685ac6d253bf351a8ca607d4368c00c5ff
zilola <zilolanazarova1994@gmail.com>
2024-04-21 05:47:31 +0500

    Add more actions to users controller, add corresponding routes, check responses in Postman, delete the wrong validation from users model

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,31 @@
-class Api::V1::UsersController < ApplicationController
+class Api::V1::UsersController < Api::V1::ApplicationController
   def index
-    @users = User.all
+    @users = User.all.order(id: :asc)
     render json: @users, status: :ok
+  end
+
+  def destroy
+    @user = set_user
+    if @user.destroy
+      render json: { success: 'user deleted' }, status: :ok
+    else
+      render json: { error: 'user not deleted' }, status: :not_found
+    end
+  end
+
+  def update
+    @users = set_user
+    if @user.update!(blocked: !@user.blocked)
+      render json: { success: 'Status updated' }, status: :ok
+    else
+      render json: { errors: @user.errors.full_messages },
+             status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get 'greeting' => 'greetings#random_greeting'
-      get 'user' => 'users#index'
+      resources :users, only: [:index, :destroy, :update]
     end
   end
   # Defines the root path route ("/")
