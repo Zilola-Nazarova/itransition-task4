bf0c641eafb124e8bd7df347fb9d7cf30360587c
zilola <zilolanazarova1994@gmail.com>
2024-04-21 05:44:36 +0500

    Fix CORS issues in browser

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::GreetingsController < ApplicationController
+class Api::V1::GreetingsController < Api::V1::ApplicationController
   def random_greeting
     @greeting = Greeting.find(Greeting.pluck(:id).sample)
     render json: @greeting, status: :ok
