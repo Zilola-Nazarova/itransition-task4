59b3710845273ab367d8d62cdee869d9bea1908d
zilola <zilolanazarova1994@gmail.com>
2024-04-26 04:11:53 +0500

    Pass user's token when rendering TableData component, make error/loading messages stretch across the table

--- app/javascript/components/TableData.js
+++ app/javascript/components/TableData.js
@@ -3,50 +3,48 @@ import { useSelector, useDispatch } from 'react-redux';
 import { getUsers, select } from '../redux/users/usersSlice';
 
 const TableData = () => {
-  const { users, isLoading, error } = useSelector((store) => store.users);
+  const { user } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
-
+  const { users, isLoading, error } = useSelector((store) => store.users);
   useEffect(() => {
-    dispatch(getUsers());
-  }, [dispatch]);
+    dispatch(getUsers(user.token))
+  }, [dispatch, user.token]);
 
   return(
     <>
-      { isLoading && <div>Loading......</div> }
+      { isLoading && <tr><td colspan="6">Loading......</td></tr> }
       { error
-        && (
-          <p>
+        && <tr>
+          <td colspan="6">
             Something went wrong!
             <br />
             { error }
-          </p>
-        )}
-        { users
-        && users.map((user, i) => (
-            <tr key={user.id}>
-              <td class="">
-                <input
-                  class="form-check-input"
-                  type="checkbox"
-                  id={user.id}
-                  name={user.id}
-                  checked={user.checked}
-                  value={user.id}
-                  onChange={() => dispatch(select(user.id)) }
-                />
-              </td>
-              <td>
-                <label class="form-check-label" for={user.id}>
-                  { i + 1 }
-                </label>
-              </td>
-              <td>{ user.id }</td>
-              <td>{ user.name}</td>
-              <td>{ user.blocked ? 'blocked' : 'active' }</td>
-              <td>{ user.last_login }</td>
-            </tr>
-          ))
-        }
+          </td>
+        </tr>
+      }
+      { users
+      && users.map((user, i) => (
+          <tr key={user.id}>
+            <td class="">
+              <input
+                class="form-check-input"
+                type="checkbox"
+                id={user.id}
+                name={user.id}
+                checked={user.checked}
+                onChange={() => dispatch(select(user.id)) }
+              />
+            </td>
+            <td>
+              <label class="form-check-label" for={user.id}>{ i + 1 }</label>
+            </td>
+            <td>{ user.id }</td>
+            <td>{ user.name}</td>
+            <td>{ user.blocked ? 'blocked' : 'active' }</td>
+            <td>{ user.last_sign_in_at }</td>
+          </tr>
+        ))
+      }
     </>   
   )
 }
