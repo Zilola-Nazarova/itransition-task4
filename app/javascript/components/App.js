1b0376c12e444a666c3e8f26d2f8f3bb4f6a9e1a
zilola <zilolanazarova1994@gmail.com>
2024-04-25 08:27:25 +0500

    Set up react router to handle signin, signout, home and unexisting routes, add Layout to be able to display navbar

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,16 +1,22 @@
 import * as React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import ProtectedLayout from './ProtectedLayout';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+import NoPage from './NoPage';
+import Layout from './Layout';
 
-import Greeting from './Greeting';
-import Users from './Users';
-import Counter from './Counter';
-
-const App = () => (
-  <Routes>
-    <Route path="/" element={<Users />} />
-    <Route path="/counter" element={<Counter />} />
-    <Route path="/greeting" element={<Greeting />} />
-  </Routes>
-);
+const App = () => {
+  return(
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="signin" element={<SignIn />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="/" element={<ProtectedLayout />} />
+      </Route>
+      <Route path="*" element={<NoPage />} />
+    </Routes>
+  )
+};
 
 export default App;
