a3b98a4050bf9f11f4c46845261d20ddf5edcba3
zilola <zilolanazarova1994@gmail.com>
2024-05-01 20:57:52 +0500

    Add front-end validations, clear back-end error messages when component rerenders

--- app/javascript/components/SignIn.js
+++ app/javascript/components/SignIn.js
@@ -1,15 +1,16 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
-import Cookies from 'js-cookie';
-import { setToken, signin } from '../redux/auth/authenticationSlice';
+import { useNavigate } from 'react-router-dom';
+import { signin } from '../redux/auth/authenticationSlice';
 import Button from './Button';
+import Form from 'react-bootstrap/Form';
 
 const SignIn = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const formRef = useRef();
   const [authMessage, setAuthMessage] = useState('');
+  const [validated, setValidated] = useState(false);
   const status = useSelector((store) => store.auth);
   const { isLoading, error, user, message } = status;
   const sendForm = async (e) => {
@@ -21,8 +22,15 @@ const SignIn = () => {
         "password": formRef.current[1]['value'],
       },
     };
-    formRef.current.reset();
-    await dispatch(signin(user));
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.stopPropagation();
+      setValidated(true);
+    } else {
+      formRef.current.reset();
+      setValidated(false);
+      await dispatch(signin(user));   
+    }
   };
   useEffect(() => {
     if (user && user.token) {
@@ -34,28 +42,39 @@ const SignIn = () => {
       setAuthMessage(error);
     }
   }, [status]);
+  useEffect(() => {
+    setAuthMessage('');
+  }, []);
 
   return (
     <div>
       <h1>Sign In</h1>
-      <form
-        class="mx-auto my-4 w-75 py-3 px-5 bg-dark text-light border border-secondary rounded-4 d-grid gap-1"
+      <Form
+        noValidate
+        validated={validated}
+        className="mx-auto my-4 w-75 py-3 px-5 bg-dark text-light border border-secondary rounded-4 d-grid gap-1"
         onSubmit={(e) => sendForm(e)}
         ref={formRef}
         id="signin-form"
       >
         <label htmlFor="email">Email: </label>
-        <input name="email" id="email" placeholder="name@example.com" />
+        <Form.Control name="email" type="email" id="email" placeholder="name@example.com" required/>
+        <Form.Control.Feedback type="invalid">
+          Please enter a valid email.
+        </Form.Control.Feedback>
         <label htmlFor="password">Password: </label>
-        <input type="password" name="password" id="password"/>
+        <Form.Control type="password" name="password" id="password" required/>
+        <Form.Control.Feedback type="invalid">
+          Please enter a password.
+        </Form.Control.Feedback>
         <button
           class="btn btn-primary px-3 mt-4"
           type="submit"
         >
           Sign In
         </button>
-        <p>{authMessage}</p>
-      </form>
+        <p class="text-danger">{authMessage}</p>
+      </Form>
       <p class="m-1">Don't have an account yet?</p>
       <Button link="/signup" style="secondary">Sign Up</Button>
     </div>
--- app/javascript/components/SignUp.js
+++ app/javascript/components/SignUp.js
@@ -1,14 +1,16 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { signup } from '../redux/auth/authenticationSlice';
 import Button from './Button';
+import Form from 'react-bootstrap/Form';
 
 const SignUp = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const formRef = useRef();
   const [authMessage, setAuthMessage] = useState('');
+  const [validated, setValidated] = useState(false);
   const status = useSelector((store) => store.auth);
   const { isLoading, error, message } = status;
   const sendForm = async (e) => {
@@ -21,35 +23,56 @@ const SignUp = () => {
         "password": formRef.current[2]['value'],
       },
     };
-    formRef.current.reset();
-    await dispatch(signup(user));
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.stopPropagation();
+      setValidated(true);
+    } else {
+      formRef.current.reset();
+      setValidated(false);
+      await dispatch(signup(user));
+    }
   };
   useEffect(() => {
     if (message) {
       setAuthMessage(message);
-      navigate("/auth/signin");
+      navigate("/signin");
     } else if (isLoading) {
       setAuthMessage("pending");
     } else if (error) {
-      setAuthMessage(error);
+      setAuthMessage(error[0]);
     }   
   }, [status]);
+  useEffect(() => {
+    setAuthMessage('');
+  }, []);
 
   return (
     <div>
       <h1>Sign Up</h1>
-      <form
-        class="mx-auto my-4 w-75 py-3 px-5 bg-dark text-light border border-secondary rounded-4 d-grid gap-1"
+      <Form
+        noValidate
+        validated={validated}
+        className="mx-auto my-4 w-75 py-3 px-5 bg-dark text-light border border-secondary rounded-4 d-grid gap-1"
         onSubmit={(e) => sendForm(e)}
         ref={formRef}
         id="signin-form"
       >
         <label htmlFor="name">Name: </label>
-        <input name="name" id="name" placeholder="John Doe" />
+        <Form.Control name="name" id="name" placeholder="John Doe" required />
+        <Form.Control.Feedback type="invalid">
+          Please enter a username.
+        </Form.Control.Feedback>
         <label htmlFor="email">Email: </label>
-        <input name="email" id="email" placeholder="name@example.com" />
+        <Form.Control name="email" id="email" type="email" placeholder="name@example.com" required />
+        <Form.Control.Feedback type="invalid">
+          Please enter a valid email.
+        </Form.Control.Feedback>
         <label htmlFor="password">Password: </label>
-        <input name="password" id="password" />
+        <Form.Control type="password" name="password" id="password" required/>
+        <Form.Control.Feedback type="invalid">
+          Please create a password.
+        </Form.Control.Feedback>
         <button
           class="btn btn-primary px-3 mt-4"
           type="submit"
@@ -57,8 +80,8 @@ const SignUp = () => {
         >
           Sign Up
         </button>
-        <p>{authMessage}</p>
-      </form>
+        <p className="text-danger">{authMessage}</p>
+      </Form>
       <p class="m-1">Already signed up?</p>
       <Button link="/signin" style="secondary">Sign In</Button>
     </div>
